437b4b25082cbd4401d92aa2e0951105f37cfde8
Rajeev <rajeevkr8917@gmail.com>
2022-07-18 17:30:19 +0530

    dependency added

--- LookUp/settings.py
+++ LookUp/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social',
-    'accounts.apps.AccountsConfig',
+    
     'django_filters',
     'storages',
 ]
